d60f3ebdb30e2deada93891d42d96ac4f78c5042
Umar Hayat Ali <ummarh760@gmail.com>
2026-08-14 19:38:47 +0500

    Cloudflare link changed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://cities-cups-brian-quickly.trycloudflare.com";
+const API_BASE_URL = "https://sample-atlantic-aids-indexes.trycloudflare.com";
 let pageGroups=[], currentIndex=0, currentCoords=[], currentImg=null;
 let currentDigitalData = null;
 let currentSearchQuery = '';
