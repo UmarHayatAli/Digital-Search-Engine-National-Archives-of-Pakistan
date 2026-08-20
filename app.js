043d2b6497eb4863516814a1de487f3d4b7bdbfd
Umar Hayat Ali <ummarh760@gmail.com>
2026-08-20 16:14:56 +0500

    Cloudflare Link Changed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://loves-lending-quantitative-endangered.trycloudflare.com";
+const API_BASE_URL = " https://parenting-verification-blah-thee.trycloudflare.com";
 // const API_BASE_URL = "";
 let pageGroups=[], currentIndex=0, currentCoords=[], currentImg=null;
 let currentDigitalData = null;
